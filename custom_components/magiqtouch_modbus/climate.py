d2303ae41dde4723e48d5fe48fc3a82fa6a39e81
mitchell <m@mrhsystems.com>
2025-03-27 20:36:04 +1100

    Add swing mode to fan for external/recycle modes.

--- custom_components/magiqtouch_modbus/climate.py
+++ custom_components/magiqtouch_modbus/climate.py
@@ -87,17 +87,17 @@ class MagiqtouchZone(ClimateEntity):
         self._attr_swing_mode = None
         self._attr_swing_modes = None       
         self._attr_hvac_modes = supportedmodes
-        
-        
 
     #Status update.
     async def async_update(self):
         data = await fetch_hvac_status(self.api_url)
         self.systemmode = data.get('system_mode') #systemmode not part of climate class.
-        if self.systemmode == 3: #Cooler mode - temp mode
-            self._attr_fan_mode = "Temperature"
-        else:
-            self._attr_fan_mode = self._map_fanspeed(data.get('evap_fanspeed'),data.get('heater_fanspeed'))
+       
+        #Occasional error invalid fanspeed mode occuring when switching external/interanl modes.
+        evap_fanspeed = data.get('evap_fanspeed')
+        heat_fanspeed = data.get('heater_fanspeed')
+        currentfanmode = self._map_fanspeed(evap_fanspeed,heat_fanspeed)
+        self._attr_fan_mode = currentfanmode
         
         #System Mode.
         current_status_systemmode = self._map_mode(self.systemmode, data.get('system_power')) #System Mode.
@@ -113,6 +113,12 @@ class MagiqtouchZone(ClimateEntity):
                 self._attr_swing_mode = "Zone Closed"
             elif self.heatzonepower == 1:
                 self._attr_swing_mode = "Zone Open"
+        elif self._attr_hvac_mode == HVACMode.FAN_ONLY and HVACMode.COOL in self._attr_hvac_modes and HVACMode.HEAT in self._attr_hvac_modes:
+            self._attr_swing_modes = ["External","Recycle"]
+            if self.systemmode == 0:
+                self._attr_swing_mode = "External"
+            elif self.systemmode == 1:
+                self._attr_swing_mode = "Recycle"          
         else:
              self._attr_swing_mode = None
              self._attr_swing_modes = None
@@ -127,8 +133,7 @@ class MagiqtouchZone(ClimateEntity):
             self._attr_target_temperature = data.get(targetzonekey)
 
         self.async_write_ha_state()
-        
-    
+
     #Command Section
     async def send_hvac_command(self, payload):
         async with aiohttp.ClientSession() as session:
@@ -147,6 +152,8 @@ class MagiqtouchZone(ClimateEntity):
     async def async_set_hvac_mode(self, new_hvac_mode: str):
         if new_hvac_mode == HVACMode.FAN_ONLY:
             await self.send_hvac_command("mode=0")
+        elif new_hvac_mode == HVACMode.FAN_ONLY:
+            await self.send_hvac_command("mode=1")
         elif new_hvac_mode == HVACMode.COOL:
             await self.send_hvac_command("mode=2")
         elif new_hvac_mode == HVACMode.HEAT:
@@ -200,8 +207,11 @@ class MagiqtouchZone(ClimateEntity):
         if swing_mode == "Zone Open":
             await self.send_hvac_command(f"zone{self.zone}=on")        
         elif swing_mode == "Zone Closed":
-            await self.send_hvac_command(f"zone{self.zone}=off")        
-        
+            await self.send_hvac_command(f"zone{self.zone}=off")            
+        elif swing_mode == "External":
+            await self.send_hvac_command("mode=0")
+        elif swing_mode == "Recycle":
+            await self.send_hvac_command("mode=1")
 
     def _map_mode(self, mode, system_power):
         # Mapping system mode to Home Assistant modes
@@ -224,10 +234,20 @@ class MagiqtouchZone(ClimateEntity):
         return None
 
     def _map_fanspeed(self, coolerfanspeed,heaterfanspeed):
-        if self._attr_hvac_mode == HVACMode.HEAT:
+        if self.systemmode == 0 and coolerfanspeed > 0:
+            return coolerfanspeed
+        elif self.systemmode == 1 and heaterfanspeed > 0: #Recycle Fan, use heater fanspeed.
             return heaterfanspeed
-        return coolerfanspeed
-
+        elif self.systemmode == 2 and coolerfanspeed > 0:
+            return coolerfanspeed
+        elif self.systemmode == 3: #Cooler mode - temp mode
+            return "Temperature"
+        elif self.systemmode == 4 and heaterfanspeed > 0:
+            return heaterfanspeed
+        _LOGGER.error(f"Could not map fan speed. SystemMode is {str(self.systemmode)} coolerfanspeed: {str(coolerfanspeed)} heaterfanspeed: {str(heaterfanspeed)}")
+        return None
+        
+        
     #@property
     #def unique_id(self) -> str:
         #return uid
